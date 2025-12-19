1dceeb2927e75b3b4202e6ef818d9529b20851cb
Jaba-Akter <infojeba218@gmall.com>
2025-12-19 19:50:59 +0600

    branch create done

--- src/Pages/AddProperty.jsx
+++ src/Pages/AddProperty.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AddProperty = () => {
     return (
         <div>
-            this is Add Property page
+            this is Add Property 
         </div>
     )
 }
